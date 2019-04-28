aac61aa886f83b63a7eaa96301829230d0d965a3
Manvinder Singh <manvinder.singh60@gmail.com>
2019-04-28 12:55:01 +0530

    moved words data file

--- src/word_generator.py
+++ src/word_generator.py
@@ -1,9 +1,9 @@
 from random import randint
 
-file_name = 'sowpods.txt'
+file_path = '../data/sowpods.txt'
 
 def get_random_word():
-    with open(file_name) as f:
+    with open(file_path) as f:
         lines = f.readlines()
         rand_int = randint(0, len(lines))
 
